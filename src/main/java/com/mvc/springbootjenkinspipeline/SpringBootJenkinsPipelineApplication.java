bdc47b0ddf771658602a46dc8e93623b98cd5ce6
rodrigomirazo <rodrigomirazo@gmail.com>
2018-03-28 15:09:17 -0600

    Initial Commit

---
+++ src/main/java/com/mvc/springbootjenkinspipeline/SpringBootJenkinsPipelineApplication.java
@@ -0,0 +1,12 @@
+package com.mvc.springbootjenkinspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsPipelineApplication.class, args);
+	}
+}
