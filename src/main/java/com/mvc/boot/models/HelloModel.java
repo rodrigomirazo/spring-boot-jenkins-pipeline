9840d661509aef73be051d3ba258c101d33b3920
miro8001 <rodrigo.mirazo.consultant@nielsen.com>
2018-05-31 10:34:52 -0500

    Remove all sign of data persistence on pom and models

--- src/main/java/com/mvc/boot/models/HelloModel.java
+++ src/main/java/com/mvc/boot/models/HelloModel.java
@@ -1,16 +1,7 @@
 package com.mvc.boot.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Id;
-
-import java.util.Date;
-
-@Entity
-@Table(name = "Hello")
 public class HelloModel {
 
-    @Id
     private Long id;
 
     String greeting;
--- src/main/java/com/mvc/boot/repository/HelloRepository.java
+++ src/main/java/com/mvc/boot/repository/HelloRepository.java
@@ -1,10 +1,5 @@
 package com.mvc.boot.repository;
 
-import java.util.List;
+public interface HelloRepository {
 
-import com.mvc.boot.models.HelloModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HelloRepository  extends CrudRepository<HelloModel, Long> {
-    List<HelloModel> findByGreeting(String greeting);
 }
